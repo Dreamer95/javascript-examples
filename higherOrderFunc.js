0e0424ed622ed075ba25d00733a53431c4316674
Đông Đông <ngocdong561995@gmail.com>
2020-03-01 05:20:11 +0000

    new commoit

--- higherOrderFunc.js
+++ higherOrderFunc.js
@@ -1,1 +1,17 @@
+// higher Order Function
+function cha () {
+  let x = 10;
+  return funtion con (p) {
+    x++;
+    return x + p;
+  }
+}
 
+console.log(cha()(1))
+console.log(cha()(1))
+
+const bien = cha();
+
+console.log(bien(1));
+console.log(bien(1));
+console.log(bien(1));
